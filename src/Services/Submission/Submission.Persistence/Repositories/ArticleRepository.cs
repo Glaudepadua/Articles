4bec100042c9b5a8fc6d8f812c7565e45bbeb7c9
glaudepadua <glaudepadua@gmail.com>
2026-07-06 22:21:10 -0300

    CreateArticle – Implement Generic & Domain Repositories

---
+++ src/Services/Submission/Submission.Persistence/Repositories/ArticleRepository.cs
@@ -0,0 +1,9 @@
+﻿using Submission.Domain.Entities;
+
+namespace Submission.Persistence.Repositories;
+
+public class ArticleRepository(SubmissionDbContext dbContext) 
+    : Repository<Article>(dbContext)
+{
+
+}
