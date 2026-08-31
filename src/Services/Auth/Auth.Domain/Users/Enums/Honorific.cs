858901f48ba1dff46bc4dd6ab8066bd126eddb1d
glaudepadua <glaudepadua@gmail.com>
2026-08-31 17:14:42 -0300

    feat: CreateUser – Implement User Domain Aggregate with DDD Principles

---
+++ src/Services/Auth/Auth.Domain/Users/Enums/Honorific.cs
@@ -0,0 +1,9 @@
+﻿namespace Auth.Domain.Users.Enums;
+
+public enum Honorific
+{
+    Ms,
+    Mrs,
+    Dr,
+    Prof
+}
---
+++ src/Services/Auth/Auth.Domain/Users/ValueObjects/HonorificTitle.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Versioning;
+using Blocks.Core;
+using Blocks.Domain.ValueObjects;
+
+namespace Auth.Domain.Users.ValueObjects;
+
+public class HonorificTitle : StringValueObject
+{
+    private HonorificTitle(string value) => Value = value;
+
+    public static HonorificTitle FromString(string honorific)
+    {
+        Guard.ThrowIfNullOrWhiteSpace(honorific);
+        return new HonorificTitle(honorific.Trim());
+    }
+
+    public static HonorificTitle? FromEnum(Honorific? honorific)
+    {
+        if (honorific.HasValue)
+        {
+            return new HonorificTitle(honorific!.ToString());
+        }
+
+        return null;
+    }
+}
